1e4864f2a85317d6fc74457d340de0e405250560
rerich3 <miroslav.imris@gmail.com>
2019-10-24 09:03:20 +0200

    Pythagoras

--- pythagoras.php
+++ pythagoras.php
@@ -2,4 +2,10 @@
 $a = 5 ;
 $b = 4 ;
 $obdelnikobsah = $a * $b ;
-echo 'Obsah obdelníku o stranách ' . $a . 'cm,' . $b . 'cm ' . 'je ' . $obdelnikobsah . 'cm2.' ;
+echo '<p>Obsah obdelníku o stranách ' . $a . 'cm,' . $b . 'cm ' . 'je ' . $obdelnikobsah . 'cm².</p>' ;
+
+$strana = 6 ;
+$uhel = 60 ;
+$vyska = $strana * sin(deg2rad($uhel))  ;
+$trojuhelnikobsah = round($strana * $vyska / 2) ;
+echo  '<p>Rovnostranný trojúhelník o straně délky ' . $strana . ' cm '. 'a uhlu ' . $uhel . '° je ' . $trojuhelnikobsah . 'cm².</p>' ;
